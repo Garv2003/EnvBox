70c832fcf3423e81d784357108cc1fc1d94c6411
Garv2003 <aggarwalgarv0505@gmail.com>
2024-12-23 00:52:15 +0530

    basic stepup

---
+++ app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+console.log("SSR")
+
+export default function Home() {
+  return <Welcome />;
+}
